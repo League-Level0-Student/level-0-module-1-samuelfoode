3e1bd891907e4e6504cd374a367a641aafbc5ba0
Your Name <you@example.com>
2025-09-12 17:02:43 -0700

    message

--- src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
+++ src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
@@ -15,18 +15,25 @@ import javax.swing.JOptionPane;
  */
 
 public class SecretMessageBox {
-
+	public static void main(String[] args) {
 	// 0. Make a main method and put steps 1-5 inside it
 	
 	// 1. Set a password in a String variable
-	
+	String pass = "password";
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
+	String message = JOptionPane.showInputDialog("what is the message");
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
+	String pass2 = JOptionPane.showInputDialog("what is the other guys pass"); 
 
 	// 4. If their guess matches the password, show them the secret message
-
+	if (pass2.equalsIgnoreCase(pass)) {
+		JOptionPane.showMessageDialog(null,message);
+		
+	}
+	else{
+		JOptionPane.showMessageDialog(null,"inteuder");
+	}
 	// 5. If the password does not match, pop-up "INTRUDER!!"
-
+	}
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -2,18 +2,31 @@ package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
-		// 1. Make a variable to hold the score
+		// 1. Make a variable to hold the scor
+		int score = 0;
 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+		String riddle[]= {"what is my birthday", "what is my dog", "do i have a dog", "is 7x7 49"};
+		String answer[]= {"november 4th", "my dog", "no", "yes"};
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		boolean bol =  true;
+		for(int i=0;i<=riddle.length; i++) {
+			String input = JOptionPane.showInputDialog(riddle[i]);
+			if(input.equalsIgnoreCase(answer[i])){
+				score++;
+				JOptionPane.showMessageDialog(null,"you got a point");
+			} else {
+				JOptionPane.showMessageDialog(null,"you didnt a point");
+			}
+			JOptionPane.showMessageDialog(null,score);
+		}
+			
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
